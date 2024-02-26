835fcc6ac86c75b58f87a33cb93629e72137d117
ApoloEvy <127549281+ApoloEvy@users.noreply.github.com>
2024-02-26 16:39:38 -0300

    Atualização Analitico.py
      Uma atualização que impede o usuário de colocsr números acima de 10.

--- analitico.py
+++ analitico.py
@@ -1,7 +1,25 @@
-numOne = int(input("Nota do primeiro bimestre "))
+
+numone = int(input("Nota do primeiro bimestre "))
+if numone >= 11:
+ print("Por favor, Numero de 0 a 10")
+ exit()
+
 numtwo = int(input("Nota do segundo bimestre "))
+if numtwo >= 11:
+ print("Por favor, Numero de 0 a 10")
+ exit()
+
+
 numtrhee = int(input("Nota do terceiro bimestre "))
+if numtrhee >= 11:
+ print("Por favor, Numero de 0 a 10")
+ exit()
+
 numfour = int(input("Nota do quarto bimestre "))
+if numfour >= 11:
+ print("Por favor, Numero de 0 a 10")
+ exit()
+
 
 finalnote = numOne + numtwo + numtrhee + numfour
 
