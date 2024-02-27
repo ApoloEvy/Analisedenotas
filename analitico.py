4c6f6105f9c4f2aaea2684a340f02be3f7d7fe90
ApoloEvy <127549281+ApoloEvy@users.noreply.github.com>
2024-02-27 08:22:47 -0300

    Atualização analitico.py
      Agora barrando por número negativo

--- analitico.py
+++ analitico.py
@@ -1,27 +1,38 @@
-
 numone = int(input("Nota do primeiro bimestre "))
+if numone < 0:
+    print("Por favor, números de 0 a 10")
+    exit()
 if numone >= 11:
  print("Por favor, Numero de 0 a 10")
  exit()
-
+        
 numtwo = int(input("Nota do segundo bimestre "))
+if numtwo < 0:
+    print("Por favor, números de 0 a 10")
+    exit()
 if numtwo >= 11:
  print("Por favor, Numero de 0 a 10")
  exit()
 
 
 numtrhee = int(input("Nota do terceiro bimestre "))
+if numtrhee < 0:
+    print("Por favor, números de 0 a 10")
+    exit()
 if numtrhee >= 11:
  print("Por favor, Numero de 0 a 10")
  exit()
 
 numfour = int(input("Nota do quarto bimestre "))
+if numfour < 0:
+    print("Por favor, números de 0 a 10")
+    exit()
 if numfour >= 11:
  print("Por favor, Numero de 0 a 10")
  exit()
 
 
-finalnote = numOne + numtwo + numtrhee + numfour
+finalnote = numone + numtwo + numtrhee + numfour
 
 if finalnote >= 20:
  print("voce foi aprovado")
